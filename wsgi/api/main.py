eddd8c10c98fde7a1538a9b33cb6004fb53b6dd6
Alireza Nourian <alireza.nournia@gmail.com>
2013-12-25 10:58:10 +0330

    use data dir as resources

--- wsgi/api/main.py
+++ wsgi/api/main.py
@@ -1,10 +1,13 @@
 
 from __future__ import unicode_literals
+import os
 from flask import Flask, request, abort
 from hazm import sent_tokenize, word_tokenize, Normalizer, Lemmatizer, POSTagger, DependencyParser
 
+resources = os.environ['OPENSHIFT_DATA_DIR']
+
 app = Flask(__name__)
-parser = DependencyParser(tagger=POSTagger())
+parser = DependencyParser(tagger=POSTagger(path_to_model=os.path.join(resources, 'persian.tagger'), path_to_jar=os.path.join(resources, 'stanford-postagger.jar')), working_dir=resources)
 
 
 @app.route('/api/parse', methods = ['POST'])
