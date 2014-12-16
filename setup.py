a163733ecc52abaf6b76b58e39afc1cd05efb017
Alireza Nourian <az.nourian@gmail.com>
2014-12-16 10:32:22 +0330

    update hazm version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(name='hazm-api',
-	version='0.3',
+	version='0.4',
 	description='Hazm API App',
 	author='Alireza Nourian',
 	author_email='az.nourian@gmail.com',
 	url='http://www.sobhe.ir/hazm/',
 	install_requires=[
 		'Flask==0.10.1',
-		'hazm==0.3',
+		'hazm==0.4',
 	]
 )
