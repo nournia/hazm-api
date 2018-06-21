561b4491cad89e28775c0a9390891315b5de1817
Alireza Nourian <az.nourian@gmail.com>
2018-06-21 12:19:00 +0430

    load hazm modules on demand

--- app.py
+++ app.py
@@ -9,11 +9,42 @@ application.config['PROPAGATE_EXCEPTIONS'] = True
 
 resources = os.environ.get('OPENSHIFT_DATA_DIR', 'resources')
 
-normalizer = Normalizer()
-lemmatizer = Lemmatizer()
-tagger = POSTagger(model=os.path.join(resources, 'postagger.model'))
-chunker = Chunker(model=os.path.join(resources, 'chunker.model'))
-parser = DependencyParser(lemmatizer=lemmatizer, tagger=tagger, working_dir=resources)
+normalizer, lemmatizer, tagger, chunker, parser = None, None, None, None, None
+
+
+def get_normalizer():
+	global normalizer
+	if not normalizer:
+		normalizer = Normalizer()
+	return normalizer
+
+
+def get_lemmatizer():
+	global lemmatizer
+	if not lemmatizer:
+		lemmatizer = Lemmatizer()
+	return lemmatizer
+
+
+def get_tagger():
+	global tagger
+	if not tagger:
+		tagger = POSTagger(model=os.path.join(resources, 'postagger.model'))
+	return tagger
+
+
+def get_chunker():
+	global chunker
+	if not chunker:
+		chunker = Chunker(model=os.path.join(resources, 'chunker.model'))
+	return chunker
+
+
+def get_parser():
+	global parser
+	if not parser:
+		parser = DependencyParser(lemmatizer=get_lemmatizer(), tagger=get_tagger(), working_dir=resources)
+	return parser
 
 
 @application.route('/api/normalize', methods=['POST'])
@@ -21,7 +52,7 @@ def normalize():
 	if 'text' not in request.form:
 		abort(400)
 
-	return normalizer.normalize(request.form['text'])
+	return get_normalizer().normalize(request.form['text'])
 
 
 @application.route('/api/tokenize', methods=['POST'])
@@ -32,26 +63,27 @@ def tokenize():
 	return json.dumps(map(word_tokenize, sent_tokenize(request.form['normalized_text'])), ensure_ascii=False)
 
 
+@application.route('/api/lemmatize', methods=['POST'])
+def lemmatize():
+	if 'tagged_text' in request.form:
+		tagged_text = json.loads(request.form['tagged_text'])
+		return json.dumps([[get_lemmatizer().lemmatize(word, tag) for word, tag in sentence] for sentence in tagged_text], ensure_ascii=False)
+
+	elif 'tokenized_text' in request.form:
+		tokenized_text = json.loads(request.form['tokenized_text'])
+		return json.dumps([[get_lemmatizer().lemmatize(word) for word in sentence] for sentence in tokenized_text], ensure_ascii=False)
+
+	else:
+		abort(400)
+
+
 @application.route('/api/tag', methods=['POST'])
 def tag():
 	if 'tokenized_text' not in request.form:
 		abort(400)
 	tokenized_text = json.loads(request.form['tokenized_text'])
 
-	return json.dumps(tagger.tag_sents(tokenized_text), ensure_ascii=False)
-
-
-@application.route('/api/lemmatize', methods=['POST'])
-def lemmatize():
-	if 'tagged_text' in request.form:
-		tagged_text = json.loads(request.form['tagged_text'])
-		return json.dumps([[lemmatizer.lemmatize(word, tag) for word, tag in sentence] for sentence in tagged_text], ensure_ascii=False)
-
-	if 'tokenized_text' in request.form:
-		tokenized_text = json.loads(request.form['tokenized_text'])
-		return json.dumps([[lemmatizer.lemmatize(word) for word in sentence] for sentence in tokenized_text], ensure_ascii=False)
-
-	abort(400)
+	return json.dumps(get_tagger().tag_sents(tokenized_text), ensure_ascii=False)
 
 
 @application.route('/api/chunk', methods=['POST'])
@@ -61,7 +93,7 @@ def chunk():
 	tagged_text = json.loads(request.form['tagged_text'])
 	tagged_text = [map(tuple, sent) for sent in tagged_text]
 
-	return json.dumps(list(map(tree2brackets, chunker.parse_sents(tagged_text))), ensure_ascii=False)
+	return json.dumps(list(map(tree2brackets, get_chunker().parse_sents(tagged_text))), ensure_ascii=False)
 
 
 @application.route('/api/parse', methods=['POST'])
@@ -70,7 +102,7 @@ def parse():
 		abort(400)
 	tagged_text = json.loads(request.form['tagged_text'])
 
-	return json.dumps([dependency_graph.to_conll(10) for dependency_graph in parser.parse_tagged_sents(tagged_text)], ensure_ascii=False)
+	return json.dumps([dependency_graph.to_conll(10) for dependency_graph in get_parser().parse_tagged_sents(tagged_text)], ensure_ascii=False)
 
 
 @application.after_request
