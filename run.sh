c6c714c1954562a62f2271cb976152d645815416
Alireza Nourian <az.nourian@gmail.com>
2020-03-08 18:02:36 +0330

    fix run script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 ps -ef | grep hazmapi | awk '{print $2}' | xargs kill -9
 
-nohup ./gunicorn --bind=0.0.0.0:5000 --workers=1 app --timeout 10 --name hazmapi --log-file - &
+nohup ./gunicorn --bind=0.0.0.0:5000 --workers=1 --threads=4 app --timeout 10 --name hazmapi --log-file - &
